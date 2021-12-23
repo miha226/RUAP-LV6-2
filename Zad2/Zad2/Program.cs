9c730b8826882aefd94636378a95ac46e6929199
michael_srempf <michael.srempf@gmail.com>
2021-12-23 14:26:07 +0100

    final

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -41,7 +41,7 @@ namespace CallRequestResponseService
                             new StringTable()
                             {
                                 ColumnNames = new string[] {"Recency", "Frequency", "Monetary", "Time", "Class"},
-                                Values = new string[,] {  { "0", "0", "0", "0", "0" },  { "0", "0", "0", "0", "0" },  }
+                                Values = new string[,] {  { "2", "4", "1", "1", "2" },  { "4", "1", "2", "2", "1" },  }
                             }
                         },
                     },
